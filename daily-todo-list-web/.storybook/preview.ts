17edf47a4ba1ff0fde1e905d09e613a9329c578d
dev2820 <second28200@gmail.com>
2023-06-25 16:23:30 +0900

    update/ add primereact indigo theme

--- daily-todo-list-web/.storybook/preview.ts
+++ daily-todo-list-web/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/react";
 import "../src/global.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 
 const preview: Preview = {
   parameters: {
