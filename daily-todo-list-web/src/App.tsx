077559c2e1e6850e972d569ad78b4eab62d04252
dev2820 <second28200@gmail.com>
2023-06-27 16:01:35 +0900

    refactor/ change property name

--- daily-todo-list-web/src/App.tsx
+++ daily-todo-list-web/src/App.tsx
@@ -5,13 +5,13 @@ import { cva } from "class-variance-authority";
 function App() {
   const dailyTodoList = useDailyTodoList();
   const todoNotes = [
-    { title: "월요일", todoListHook: dailyTodoList.mon },
-    { title: "화요일", todoListHook: dailyTodoList.tue },
-    { title: "수요일", todoListHook: dailyTodoList.wed },
-    { title: "목요일", todoListHook: dailyTodoList.thr },
-    { title: "금요일", todoListHook: dailyTodoList.fri },
-    { title: "토요일", todoListHook: dailyTodoList.sat },
-    { title: "일요일", todoListHook: dailyTodoList.sun },
+    { title: "월요일", todoList: dailyTodoList.mon },
+    { title: "화요일", todoList: dailyTodoList.tue },
+    { title: "수요일", todoList: dailyTodoList.wed },
+    { title: "목요일", todoList: dailyTodoList.thr },
+    { title: "금요일", todoList: dailyTodoList.fri },
+    { title: "토요일", todoList: dailyTodoList.sat },
+    { title: "일요일", todoList: dailyTodoList.sun },
   ];
   const style = {
     height: "500px",
@@ -24,7 +24,7 @@ function App() {
           className={todoNoteStyle()}
           style={style}
           title={todoNote.title}
-          {...todoNote.todoListHook}
+          {...todoNote.todoList}
         ></TodoNote>
       ))}
     </div>
