c4f4616a546f40386cba8445a4fecafeec3aec06
dev2820 <second28200@gmail.com>
2023-06-27 16:04:52 +0900

    refactor/ change code order

--- daily-todo-list-web/src/services/daily-todo-service/index.ts
+++ daily-todo-list-web/src/services/daily-todo-service/index.ts
@@ -23,38 +23,39 @@ const setItem = (key: string, data: Todo[]) => {
 
   return true;
 };
+
+const read = (day: Day) => getItem(`todo-${day}`);
+
+const readAll = () => ({
+  mon: readMon(),
+  tue: readTue(),
+  wed: readWed(),
+  thr: readThr(),
+  fri: readFri(),
+  sat: readSat(),
+  sun: readSun(),
+});
+
+const readMon = () => read(DAY.MON);
+const readTue = () => read(DAY.TUE);
+const readWed = () => read(DAY.WED);
+const readThr = () => read(DAY.THR);
+const readFri = () => read(DAY.FRI);
+const readSat = () => read(DAY.SAT);
+const readSun = () => read(DAY.SUN);
+
+const write = (day: Day, newTodoList: Todo[]) =>
+  setItem(`todo-${day}`, newTodoList);
+
+const writeMon = (newTodoList: Todo[]) => write(DAY.MON, newTodoList);
+const writeTue = (newTodoList: Todo[]) => write(DAY.TUE, newTodoList);
+const writeWed = (newTodoList: Todo[]) => write(DAY.WED, newTodoList);
+const writeThr = (newTodoList: Todo[]) => write(DAY.THR, newTodoList);
+const writeFri = (newTodoList: Todo[]) => write(DAY.FRI, newTodoList);
+const writeSat = (newTodoList: Todo[]) => write(DAY.SAT, newTodoList);
+const writeSun = (newTodoList: Todo[]) => write(DAY.SUN, newTodoList);
+
 export const useDailyTodoService = () => {
-  const read = (day: Day) => getItem(`todo-${day}`);
-
-  const readAll = () => ({
-    mon: readMon(),
-    tue: readTue(),
-    wed: readWed(),
-    thr: readThr(),
-    fri: readFri(),
-    sat: readSat(),
-    sun: readSun(),
-  });
-
-  const readMon = () => read(DAY.MON);
-  const readTue = () => read(DAY.TUE);
-  const readWed = () => read(DAY.WED);
-  const readThr = () => read(DAY.THR);
-  const readFri = () => read(DAY.FRI);
-  const readSat = () => read(DAY.SAT);
-  const readSun = () => read(DAY.SUN);
-
-  const write = (day: Day, newTodoList: Todo[]) =>
-    setItem(`todo-${day}`, newTodoList);
-
-  const writeMon = (newTodoList: Todo[]) => write(DAY.MON, newTodoList);
-  const writeTue = (newTodoList: Todo[]) => write(DAY.TUE, newTodoList);
-  const writeWed = (newTodoList: Todo[]) => write(DAY.WED, newTodoList);
-  const writeThr = (newTodoList: Todo[]) => write(DAY.THR, newTodoList);
-  const writeFri = (newTodoList: Todo[]) => write(DAY.FRI, newTodoList);
-  const writeSat = (newTodoList: Todo[]) => write(DAY.SAT, newTodoList);
-  const writeSun = (newTodoList: Todo[]) => write(DAY.SUN, newTodoList);
-
   return {
     read,
     readAll,
