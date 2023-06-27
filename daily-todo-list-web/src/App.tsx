5e03e57019c6c808118a57a3292e630d4acd5d91
dev2820 <second28200@gmail.com>
2023-06-27 10:59:14 +0900

    refactor/ use map to create daily todo list

--- daily-todo-list-web/src/App.tsx
+++ daily-todo-list-web/src/App.tsx
@@ -4,55 +4,29 @@ import { cva } from "class-variance-authority";
 
 function App() {
   const dailyTodoList = useDailyTodoList();
-
+  const todoNotes = [
+    { title: "월요일", todoListHook: dailyTodoList.mon },
+    { title: "화요일", todoListHook: dailyTodoList.tue },
+    { title: "수요일", todoListHook: dailyTodoList.wed },
+    { title: "목요일", todoListHook: dailyTodoList.thr },
+    { title: "금요일", todoListHook: dailyTodoList.fri },
+    { title: "토요일", todoListHook: dailyTodoList.sat },
+    { title: "일요일", todoListHook: dailyTodoList.sun },
+  ];
   const style = {
     height: "500px",
   };
 
   return (
     <div className={todoNoteListStyle()}>
-      <TodoNote
-        className={todoNoteStyle()}
-        style={style}
-        title="월요일"
-        todoListHook={dailyTodoList.mon}
-      ></TodoNote>
-      <TodoNote
-        className={todoNoteStyle()}
-        style={style}
-        title="화요일"
-        todoListHook={dailyTodoList.tue}
-      ></TodoNote>
-      <TodoNote
-        className={todoNoteStyle()}
-        style={style}
-        title="수요일"
-        todoListHook={dailyTodoList.wed}
-      ></TodoNote>
-      <TodoNote
-        className={todoNoteStyle()}
-        style={style}
-        title="목요일"
-        todoListHook={dailyTodoList.thr}
-      ></TodoNote>
-      <TodoNote
-        className={todoNoteStyle()}
-        style={style}
-        title="금요일"
-        todoListHook={dailyTodoList.fri}
-      ></TodoNote>
-      <TodoNote
-        className={todoNoteStyle()}
-        style={style}
-        title="토요일"
-        todoListHook={dailyTodoList.sat}
-      ></TodoNote>
-      <TodoNote
-        className={todoNoteStyle()}
-        style={style}
-        title="일요일"
-        todoListHook={dailyTodoList.sun}
-      ></TodoNote>
+      {todoNotes.map((todoNote) => (
+        <TodoNote
+          className={todoNoteStyle()}
+          style={style}
+          title={todoNote.title}
+          todoListHook={todoNote.todoListHook}
+        ></TodoNote>
+      ))}
     </div>
   );
 }
