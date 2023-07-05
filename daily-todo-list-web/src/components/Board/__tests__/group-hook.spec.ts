25d7b685112b5f13d9f53f23e83995faf58b7840
dev2820 <second28200@gmail.com>
2023-07-05 22:49:35 +0900

    feat/ group-hook

---
+++ daily-todo-list-web/src/components/Board/__tests__/group-hook.spec.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from "vitest";
+import { useGroup, type Identifiable } from "../group-hook";
+import { renderHook, act } from "@testing-library/react-hooks";
+
+describe("useGroup", () => {
+  const item1: Identifiable = { id: "1" };
+  const item2: Identifiable = { id: "2" };
+  const item3: Identifiable = { id: "3" };
+
+  it("should init from list", () => {
+    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+
+    expect(result.current.items).toStrictEqual([item1, item2, item3]);
+  });
+
+  it("should be able to import groups for a particular key", () => {
+    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+
+    expect(result.current.findById("1")?.id).toBe("1");
+  });
+
+  it("should be able to insert item", () => {
+    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+    const newItem = { id: "4" };
+    act(() => result.current.insertItem(1, newItem));
+
+    expect(result.current.items).toStrictEqual([item1, newItem, item2, item3]);
+  });
+
+  it("should be able to remove item", () => {
+    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+
+    result.current.removeItem(1);
+
+    expect(result.current.items).toStrictEqual([item1, item3]);
+  });
+});
---
+++ daily-todo-list-web/src/components/Board/group-hook.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export type Id = string;
+export type Identifiable = {
+  id: Id;
+  [key: string]: unknown;
+};
+
+export const useGroup = (initialItems: Identifiable[] = []) => {
+  const [items, setItems] = useState(initialItems);
+
+  const findById = (id: Id) => items.find((item) => item.id === id);
+  const removeItem = (index: number) => {
+    setItems((items: Identifiable[]) => [
+      ...items.slice(0, index),
+      ...items.slice(index + 1),
+    ]);
+  };
+  const insertItem = (index: number, newItem: Identifiable) => {
+    setItems((items: Identifiable[]) => [
+      ...items.slice(0, index),
+      newItem,
+      ...items.slice(index),
+    ]);
+  };
+  return {
+    items: items,
+    findById,
+    removeItem,
+    insertItem,
+  };
+};
