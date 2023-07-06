155090ab4fc63c9a7d42993f66df5ccaa752d798
dev2820 <second28200@gmail.com>
2023-07-06 11:01:01 +0900

    refactor/ adapt changed useGroup interface

--- daily-todo-list-web/src/components/Group/__tests__/group-hook.spec.ts
+++ daily-todo-list-web/src/components/Group/__tests__/group-hook.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "vitest";
-import { useGroup, type Identifiable } from "../group-hook";
 import { renderHook, act } from "@testing-library/react-hooks";
+import { type Identifiable } from "@/components/types";
+import { useGroup } from "../group-hook";
 
 describe("useGroup", () => {
   const item1: Identifiable = { id: "1" };
@@ -8,25 +9,33 @@ describe("useGroup", () => {
   const item3: Identifiable = { id: "3" };
 
   it("should init from list", () => {
-    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+    const { result } = renderHook(() =>
+      useGroup("group1", [item1, item2, item3])
+    );
 
     expect(result.current.items).toStrictEqual([item1, item2, item3]);
   });
 
   it("should be able to find item by id", () => {
-    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+    const { result } = renderHook(() =>
+      useGroup("group1", [item1, item2, item3])
+    );
 
     expect(result.current.findById("1")?.id).toBe("1");
   });
 
   it("should be able to find item by index", () => {
-    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+    const { result } = renderHook(() =>
+      useGroup("group1", [item1, item2, item3])
+    );
 
     expect(result.current.findByIndex(1)?.id).toBe("2");
   });
 
   it("should be able to insert item", () => {
-    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+    const { result } = renderHook(() =>
+      useGroup("group1", [item1, item2, item3])
+    );
     const newItem = { id: "4" };
     act(() => result.current.insertItem(1, newItem));
 
@@ -34,7 +43,9 @@ describe("useGroup", () => {
   });
 
   it("should be able to remove item", () => {
-    const { result } = renderHook(() => useGroup([item1, item2, item3]));
+    const { result } = renderHook(() =>
+      useGroup("group1", [item1, item2, item3])
+    );
 
     result.current.removeItem(1);
 
